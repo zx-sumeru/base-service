23c7360b6a0726e1b1f3270791f97e3395fec50e
Zhang Xu <1243399327@qq.com>
2018-10-15 23:38:33 +0800

    Initial commit

---
+++ src/main/java/com/example/baseservice/BaseServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.baseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaseServiceApplication.class, args);
+    }
+}
